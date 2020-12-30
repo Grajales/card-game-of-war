95480b5713a2b33c8f9005350bba3defa9eda7ba
Lilana Grajales <LG29878@wdxlg29878vcg.home>
2020-12-29 23:29:40 -0500

    simplified code by adding war event counter and card index

--- src/js/script2.js
+++ src/js/script2.js
@@ -2,7 +2,7 @@ console.log("hello!");
 //Constants definition
 let rankA = [1,2,3,4,5,6,7,8,9,10,"Jack","Queen","King"];
 let scoreA =[1,2,3,4,5,6,7,8,9,10,11,12,13];
-//let scoreA =[1,1,1,1,4,3,2,2,2,2,3,3,3];
+//let scoreA =[1,1,1,1,1,1,1,1,1,1,2,2,2];
 let suits = ["hearts", "spades", "clubs", "diamonds"];
 // Deck function
 // Creates a deck of cards and shuffles them 1000 times 
@@ -39,7 +39,7 @@ let suits = ["hearts", "spades", "clubs", "diamonds"];
     this.cards=cardN;
     }
   }
-// Class Game plays the game given two players with cards
+
 class Game{ 
   constructor(player1,player2){
     this.player1 =player1;
@@ -48,61 +48,55 @@ class Game{
    showOfCards() {
      let flag =0;
      let m =0;
-     for (let i=0; i<500; i++) {
+     for (let i=0; i<200; i++) {
     let p1c1 = this.player1.cards[m];
     let p2c1 = this.player2.cards[m];
-    console.log(this.player1.name);
     console.log(p1c1);
-    console.log(this.player2.name);
     console.log(p2c1);
- 
-    if (p1c1.Score !== p2c1.Score) {
+
+    if (p1c1.Score !== p2c1.Score){
         for (let k=0; k< (m+1); k++) {
+            console.log("m = " + m)
             this.notEqual(p1c1,p2c1);
             }
         
-      flag = 0;
-      m = 0;
+    flag = 0;
+    m = 0;
     }
-      else {console.log ("War!! both players have the same score! ")
+      else {
         if ((this.player1.cards.length > (4+m)) && (this.player2.cards.length > (4+m)) ) {
         m = 4 + (flag * 4);
         flag = flag +1 ;
-        } else {  console.log("Game is over ! " + (4+m) + " more cards are needed to solve the war, and " + this.player1.name + " has " + this.player1.cards.length + 
-        " cards, and " + this.player2.name + " has " + this.player2.cards.length + " cards");
-       return }
+        } else {  console.log("game is over, there are not enough cards to solve the match")}
       }
 
     if (this.player1.cards.length == 0 || this.player2.cards.length == 0 ) {
-      if (this.player1.cards.length > this.player2.cards/length) {
-        console.log(this.player1.name + " wins!!");
-        return
-        } else { console.log(this.player2.name + " wins!!") 
-      return }
+      if (this.player1.cards.length> this.player2.cards/length) {
+        console.log(this.player1.name + " wins!!")
+        } else { console.log(this.player2.name + " wins!!")  }
     }
    } // end of for loop
   }
-   notEqual(p1,p2){
-     //console.log("Player1: " + p1.Score + " Computer: " + p2.Score)
+   notEqual(p1,p2,loop){
+     console.log("Not equal cards, Player1: " + p1.Score + " Computer: " + p2.Score)
      if (p1.Score < p2.Score){ 
       this.player2.cards.push(this.player2.cards.shift())
       this.player2.cards.push(this.player1.cards.shift());
-      console.log(this.player1.name + " lost this round and it has  " + this.player1.cards.length + " cards now ")
-      console.log(this.player2.name + " won this round and it has " + this.player2.cards.length + " cards now ")  
+      console.log(this.player1.name + " Looses 1 card in this round and it has  " + this.player1.cards.length + " cards")
+      console.log(this.player2.name + " wins this round and it has " + this.player2.cards.length + " cards")  
     } else if (p1.Score > p2.Score) { 
         this.player1.cards.push(this.player1.cards.shift());
         this.player1.cards.push(this.player2.cards.shift());
-        console.log(this.player1.name + " won this round and it has " + this.player1.cards.length + " cards now")
-        console.log(this.player2.name + " lost this round and it has " +this.player2.cards.length + " cards")
+        console.log(this.player1.name + " wins this round and has " + this.player1.cards.length + " cards")
+        console.log(this.player2.name + " Looses 1 card in this round and it has " +this.player2.cards.length + " cards")
         }
       }
   }
   
-  sign = window.prompt('Are you feeling lucky? You are Player1!, click OK'); // open the window with Text "Are you feeling lucky"
+  //sign = window.prompt('Are you feeling lucky? You are Player1!'); // open the window with Text "Are you feeling lucky"
   //sign = prompt('Are you feeling lucky? You are Player1!'); // open the window with Text "Are you feeling lucky"
   //console.log(sign);
   deck1 = DeckShuffled(rankA,scoreA,suits);
-  console.log(deck1);
   let player1 = new Player("Player1");
   player1.addCards(deck1.slice(0,26));
   //console.log(player1.cards)
--- src/js/scriptLong.js
+++ src/js/scriptLong.js
@@ -2,7 +2,6 @@ console.log("hello!");
 //Constants definition
 let rankA = [1,2,3,4,5,6,7,8,9,10,"Jack","Queen","King"];
 let scoreA =[1,2,3,4,5,6,7,8,9,10,11,12,13];
-//let scoreA =[1,1,1,1,4,3,2,2,2,2,3,3,3];
 let suits = ["hearts", "spades", "clubs", "diamonds"];
 // Deck function
 // Creates a deck of cards and shuffles them 1000 times 
@@ -39,70 +38,73 @@ let suits = ["hearts", "spades", "clubs", "diamonds"];
     this.cards=cardN;
     }
   }
-// Class Game plays the game given two players with cards
+
 class Game{ 
   constructor(player1,player2){
     this.player1 =player1;
     this.player2 =player2;
   }
    showOfCards() {
-     let flag =0;
-     let m =0;
-     for (let i=0; i<500; i++) {
-    let p1c1 = this.player1.cards[m];
-    let p2c1 = this.player2.cards[m];
-    console.log(this.player1.name);
+     for (let i=0; i<100; i++) {
+    let p1c1 = this.player1.cards[0];
     console.log(p1c1);
-    console.log(this.player2.name);
+    let p2c1 = this.player2.cards[0];
     console.log(p2c1);
- 
-    if (p1c1.Score !== p2c1.Score) {
-        for (let k=0; k< (m+1); k++) {
-            this.notEqual(p1c1,p2c1);
-            }
+    if (p1c1.Score !== p2c1.Score){
+    this.notEqual(p1c1,p2c1);
+    }
+      else {
         
-      flag = 0;
-      m = 0;
-    }
-      else {console.log ("War!! both players have the same score! ")
-        if ((this.player1.cards.length > (4+m)) && (this.player2.cards.length > (4+m)) ) {
-        m = 4 + (flag * 4);
-        flag = flag +1 ;
-        } else {  console.log("Game is over ! " + (4+m) + " more cards are needed to solve the war, and " + this.player1.name + " has " + this.player1.cards.length + 
-        " cards, and " + this.player2.name + " has " + this.player2.cards.length + " cards");
-       return }
+        let p1c5 = this.player1.cards[4];
+      console.log(p1c5);
+      let p2c5 = this.player2.cards[4];
+      console.log(p2c5); 
+      if (p1c5.Score !== p2c5.Score){
+       // transfer 5 cards to the winner
+       for (let j=0; j<5; j++) {
+        this.notEqual(p1c5,p2c5);
+        }
+      } else {  
+        console.log("equal a second time equal, opening card 10") 
+        //transfer 10 cards to the winner
+        let p1c9 = this.player1.cards[8];
+        console.log(p1c9);
+        let p2c9 = this.player2.cards[8];
+        console.log(p2c9); 
+        if (p1c9.Score !== p2c9.Score){
+          // transfer 10 cards to the winner
+          for (let k=0; k<10; k++) {
+          this.notEqual(p1c9,p2c9);
+          }
+        } else { console.log("three times equal")}
       }
-
-    if (this.player1.cards.length == 0 || this.player2.cards.length == 0 ) {
-      if (this.player1.cards.length > this.player2.cards/length) {
-        console.log(this.player1.name + " wins!!");
-        return
-        } else { console.log(this.player2.name + " wins!!") 
-      return }
     }
+      //console.log(this.player1.cards)
+      //console.log(this.player2.cards)
+      console.log("this is run " + i)
    } // end of for loop
   }
    notEqual(p1,p2){
-     //console.log("Player1: " + p1.Score + " Computer: " + p2.Score)
+     console.log("Not equal cards, Player1: " + p1.Score + " Computer: " + p2.Score)
      if (p1.Score < p2.Score){ 
       this.player2.cards.push(this.player2.cards.shift())
       this.player2.cards.push(this.player1.cards.shift());
-      console.log(this.player1.name + " lost this round and it has  " + this.player1.cards.length + " cards now ")
-      console.log(this.player2.name + " won this round and it has " + this.player2.cards.length + " cards now ")  
+      console.log(this.player1.name + " Looses 1 card in this round and it has this number of cards: " + this.player1.cards.length)
+      console.log(this.player2.name + " wins this round and it has this number of cards: " + this.player2.cards.length)  
     } else if (p1.Score > p2.Score) { 
         this.player1.cards.push(this.player1.cards.shift());
         this.player1.cards.push(this.player2.cards.shift());
-        console.log(this.player1.name + " won this round and it has " + this.player1.cards.length + " cards now")
-        console.log(this.player2.name + " lost this round and it has " +this.player2.cards.length + " cards")
+        console.log(this.player1.name + " wins this round and has this number of cards: " + this.player1.cards.length)
+        console.log(this.player2.name + " Looses 1 card in this round and it has this number of cards: " +this.player2.cards.length)
         }
-      }
+        let flag = 0;
+    }
   }
   
-  sign = window.prompt('Are you feeling lucky? You are Player1!, click OK'); // open the window with Text "Are you feeling lucky"
+  //sign = window.prompt('Are you feeling lucky? You are Player1!'); // open the window with Text "Are you feeling lucky"
   //sign = prompt('Are you feeling lucky? You are Player1!'); // open the window with Text "Are you feeling lucky"
   //console.log(sign);
   deck1 = DeckShuffled(rankA,scoreA,suits);
-  console.log(deck1);
   let player1 = new Player("Player1");
   player1.addCards(deck1.slice(0,26));
   //console.log(player1.cards)
